ca73e8995b738754ec7036f460bee17f3ab45f81
g-anikin <53080894+g-anikin@users.noreply.github.com>
2020-08-31 22:56:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 #Update apt database
 sudo apt update
+#Installing git package
+sudo apt install git -y
+#Clone git-repo
+git clone https://github.com/g-anikin/check_tabs.git
+#Make files install.sh and check_tabs.py executable
+sudo chmod +x ./check_tabs/install.sh
+sudo chmod +x ./check_tabs/check_tabs.py
 #Installing pip3 module
 sudo apt install python3-pip -y
 #Intalling requirements for python3
 sudo pip3 install -r requirements.txt
-#Make file check_tabs.py executable
-sudo chmod +x check_tabs.py
