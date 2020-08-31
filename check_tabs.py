0ae0bc13cbdc36940abd74de9bea76265dcc975f
g-anikin <georgiy.anikin@gmail.com>
2020-08-31 21:26:51 +0300

    Initial commit

---
+++ check_tabs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+import pathlib
+import lz4.block
+import json
+import socket
+
+path = pathlib.Path.home().joinpath('.mozilla/firefox')
+files = path.glob('*default*/sessionstore-backups/recovery.js*')
+unique_list = []
+
+for f in files:
+    b = f.read_bytes()
+    if b[:8] == b'mozLz40\0':
+        b = lz4.block.decompress(b[8:])
+    j = json.loads(b)
+    for w in j['windows']:
+        for t in w['tabs']:
+            i = t['index'] - 1
+            site = t['entries'][i]['url'].split('/')
+            try:
+                unique_list.append(socket.gethostbyname(site[2]))
+            except IndexError:
+                pass
+unique_list = set(unique_list)
+for i in unique_list:
+    print(i)
+print(len(unique_list),'unique IP')
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Installing pip3 module
+sudo apt install python3-pip
+#Intalling requirements for python3
+sudo pip3 install -r requirements.txt
+#Make file check_tabs.py executable
+sudo chmod +x check_tabs.py
