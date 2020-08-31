845c13cacc716ad573b7f5f9957a3b6ca5276d89
g-anikin <53080894+g-anikin@users.noreply.github.com>
2020-08-31 22:29:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #Update apt database
 sudo apt update
 #Installing pip3 module
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 #Intalling requirements for python3
 sudo pip3 install -r requirements.txt
 #Make file check_tabs.py executable
